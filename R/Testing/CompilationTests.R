9e26052b91c09753ab31e07c7e594a425b55155b
Alessandro Colombi VMCatt <colombi.alessandro@hotmail.it>
2025-10-22 10:34:35 +0200

    first test from VMCatt

--- R/Testing/CompilationTests.R
+++ R/Testing/CompilationTests.R
@@ -1,4 +1,5 @@
 wd = "C:/Users/colom/DynamicFeatureAllocation/R/Testing/"
+wd = "/home/lucia.paci/Lucia/Ale/DynamicFeatureAllocation/R/Testing/"
 setwd(wd)
 Rcpp::sourceCpp("../../src/RcppFunctions.cpp")
-
+log_dnorm(0,0,1)
